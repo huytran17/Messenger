5607f3a9e3af95454a1013df3c62b795fd11b035
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-09-11 15:31:54 +0700

    create getall user controller

--- routes/user.router.js
+++ routes/user.router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { getAll } = require("../controllers/user.controller");
 
-router.get("/", (req, res) => {
-  getAll(req, res);
-});
+router.get("/", getAll);
 
 module.exports = router;
