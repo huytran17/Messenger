8945f58732d1879c47a3937d208c694e19db2a90
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-09-17 21:26:16 +0700

    add fields

---
+++ controllers/conversation.controller.js
@@ -0,0 +1,7 @@
+const Conversation = require("../models/conversation.model");
+
+module.exports.store = (req, res) => {};
+
+module.exports.update = (req, res) => {};
+
+module.exports.delete = (req, res) => {};
--- models/conversation.model.js
+++ models/conversation.model.js
@@ -2,8 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const mongoose_delete_plugin = require("../utils/Plugins/mongoseDelete.plugin");
 
-const conversationSchema = new Schema({ timestamps: true });
+const conversationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    color: {
+      type: String,
+      trim: true,
+    },
+    background: {
+      type: String,
+      alias: "bg",
+    },
+    chathead_photo_path: {
+      type: String,
+      alias: "headphoto",
+    },
+    type: {
+      type: Number,
+      default: 1,
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Không được để trống."],
+    },
+  },
+  { timestamps: true }
+);
 
-mongoose_delete_plugin(userSchema);
+mongoose_delete_plugin(conversationSchema);
 
 module.exports = mongoose.model("Conversation", conversationSchema);
