885f2c2008706c07ce80cdd62af1874a47defc51
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-09-25 23:32:54 +0700

    bottom nav

--- client/src/components/Layout/BottomNav.js
+++ client/src/components/Layout/BottomNav.js
@@ -4,11 +4,16 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import ArchiveIcon from "@mui/icons-material/Archive";
 import Paper from "@mui/material/Paper";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FolderIcon from "@mui/icons-material/Folder";
 
 export default function BottomNav() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Box sx={{ pb: 7 }}>
@@ -18,21 +23,35 @@ export default function BottomNav() {
           bottom: 0,
           left: 0,
           right: 0,
-          border: "none",
           boxShadow: "none",
         }}
         elevation={3}
       >
         <BottomNavigation
-          showLabels
+          sx={{ width: "100%" }}
           value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
+          onChange={handleChange}
         >
-          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+          <BottomNavigationAction
+            label="Recents"
+            value="recents"
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            label="Favorites"
+            value="favorites"
+            icon={<FavoriteIcon />}
+          />
+          <BottomNavigationAction
+            label="Nearby"
+            value="nearby"
+            icon={<LocationOnIcon />}
+          />
+          <BottomNavigationAction
+            label="Folder"
+            value="folder"
+            icon={<FolderIcon />}
+          />
         </BottomNavigation>
       </Paper>
     </Box>
--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,5 +1,4 @@
 export { default as Home } from "./Home";
-export { default as Footer } from "./Footer";
 export { default as ChatFrame } from "./ChatFrame";
 export { default as DrawerLeft } from "./DrawerLeft";
 export { default as DrawerRight } from "./DrawerRight";
