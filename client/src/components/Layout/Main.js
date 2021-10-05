3db62212648b51697aa55eea118469e4262cba0e
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-05 16:35:30 +0700

    padding for main content

--- client/src/components/Layout/Main.js
+++ client/src/components/Layout/Main.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import { DrawerLeft, DrawerRight, Bar, BottomNav } from "./index";
+import { View } from "../../constants/index";
 
 export default function Main({ children, ...rest }) {
   return (
@@ -10,7 +11,16 @@ export default function Main({ children, ...rest }) {
         <CssBaseline />
         <Bar />
         <DrawerLeft />
-        <Box component="main" sx={{ width: "100%", marginBottom: 10 }}>
+        <Box
+          component="main"
+          sx={{
+            width: "100%",
+            marginBottom: `${View.BOTTOM_NAV_HEIGHT + 20}px`,
+            "& > .MuiBox-root": {
+              padding: "5px",
+            },
+          }}
+        >
           {children}
         </Box>
         <DrawerRight />
