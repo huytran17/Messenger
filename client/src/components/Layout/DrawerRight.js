f3e538b9e84c2580d508edff2f714b9fd9e6a21a
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-05 13:23:03 +0700

    remove unuse import

--- client/src/components/Layout/DrawerRight.js
+++ client/src/components/Layout/DrawerRight.js
@@ -1,18 +1,15 @@
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectStatusRight,
-  toggleStatusRight,
+  toggleStatusRight
 } from "../../app/slices/appBarSlice";
 
 const drawerWidth = 200;
