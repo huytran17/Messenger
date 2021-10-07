4e7b4190beac24b40bc3472a4e83e26d43128985
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-07 20:53:20 +0700

    remove unuse file

--- client/src/components/Pages/Profile/Profile.js
+++ client/src/components/Pages/Profile/Profile.js
@@ -1,22 +1,8 @@
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import Collapse from "@mui/material/Collapse";
-import { red } from "@mui/material/colors";
-import IconButton from "@mui/material/IconButton";
 import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import * as React from "react";
-import CoverPhoto from "../../../static/images/loginPanel.jpg";
-import { ProfileTab } from "./index";
-import { Media, ProfileHeader, Content, Action, CollapseTab } from "./index";
+import { Action, CollapseTab, Content, Media, ProfileHeader } from "./index";
 
 const avatarSize = 80;
 
