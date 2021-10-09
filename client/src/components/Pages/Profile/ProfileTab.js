d86a33d1bfcbdbb5fc3e19da120471c3d6e163ed
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-09 21:07:35 +0700

    remove unuse import

--- client/src/components/Pages/Profile/ProfileTab.js
+++ client/src/components/Pages/Profile/ProfileTab.js
@@ -17,26 +17,6 @@ const tabIndex = {
   sec: 1,
 };
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Box>{children}</Box>
-        </Box>
-      )}
-    </Box>
-  );
-}
-
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
--- client/src/components/Pages/Profile/SecurityTab.js
+++ client/src/components/Pages/Profile/SecurityTab.js
@@ -1,40 +1,16 @@
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import DatePicker from "@mui/lab/DatePicker";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import axios from "axios";
-import moment from "moment";
 import PropTypes from "prop-types";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
 import {
   changeData,
   selectData,
   selectError,
-  selectIsAllValid,
   validate,
-  setData,
 } from "../../../app/slices/securitySlice";
-import { Field, Server } from "../../../constants/index";
-import {
-  CommonFormControl,
-  PasswordField,
-  CommonTextField,
-  FormGridItem,
-  TabPanel,
-} from "../../index";
-import { localeMap, maskMap } from "../../utils/index";
-
-const XFormGridItem = ({ errorField, ...rest }) => {
-  return (
-    <FormGridItem errorField={errorField} errSx={{ marginLeft: 1 }} {...rest} />
-  );
-};
+import { Field } from "../../../constants/index";
+import { PasswordField, TabPanel } from "../../index";
 
 const errSx = { marginLeft: 1 };
 
