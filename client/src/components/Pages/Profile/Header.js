2670a53abe69b28f280fe4f50e4f826eaffc2702
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-11 22:29:53 +0700

    form upload avatar

--- client/src/components/Pages/Profile/Header.js
+++ client/src/components/Pages/Profile/Header.js
@@ -2,14 +2,20 @@ import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import { Typography } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Badge from "@mui/material/Badge";
+import Button from "@mui/material/Button";
 import CardHeader from "@mui/material/CardHeader";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 import Skeleton from "@mui/material/Skeleton";
 import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectData, selectIsLoading } from "../../../app/slices/userSlice";
 import { STRING, View } from "../../../constants/index";
-import React, { useState } from "react";
-import { UploadAvatar } from "./index";
 
 const Header = styled(CardHeader)(({ theme }) => ({
   display: "flex",
@@ -20,16 +26,41 @@ const Header = styled(CardHeader)(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const SmallCamera = styled(CameraAltIcon)({
-  cursor: "pointer",
-});
+const SmallCamera = styled(CameraAltIcon)(({ theme }) => ({
+  display: "none",
+}));
 
-export default function ProfileHeader({ ...props }) {
+const BadgeContainer = styled(Badge)(({ theme }) => ({
+  "&:hover": {
+    "& .MuiSvgIcon-root": {
+      display: "block",
+    },
+  },
+}));
+
+export default function ProfileHeader({
+  submitBtnLabel,
+  cancelBtnLabel,
+  uploadTitle,
+  ...props
+}) {
   const user = useSelector(selectData);
 
   const isLoading = useSelector(selectIsLoading);
 
   const [isShowFormUpload, setShowFormUpload] = useState(false);
+
+  const handleOpenFormUpload = () => {
+    setShowFormUpload(true);
+  };
+
+  const handleCloseFormUpload = () => {
+    setShowFormUpload(false);
+  };
+
+  const onChange = (event) => {
+    console.log(event.target.files[0]);
+  };
 
   return (
     <>
@@ -42,12 +73,10 @@ export default function ProfileHeader({ ...props }) {
               height={View.AVATAR_SIZE}
             />
           ) : (
-            <Badge
+            <BadgeContainer
               overlap="circular"
               anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              badgeContent={
-                <SmallCamera onClick={() => setShowFormUpload(true)} />
-              }
+              badgeContent={<SmallCamera onClick={handleOpenFormUpload} />}
             >
               <Avatar
                 sx={{
@@ -62,7 +91,7 @@ export default function ProfileHeader({ ...props }) {
                 }
                 alt={user.username}
               ></Avatar>
-            </Badge>
+            </BadgeContainer>
           )
         }
         title={
@@ -80,7 +109,45 @@ export default function ProfileHeader({ ...props }) {
         }
         {...props}
       />
-      <UploadAvatar isShow={isShowFormUpload} handleShow={setShowFormUpload} />
+
+      <Dialog open={isShowFormUpload} onClose={handleCloseFormUpload}>
+        <DialogTitle>{uploadTitle}</DialogTitle>
+        <DialogContent>
+          <Avatar
+            sx={{
+              width: View.AVATAR_SIZE * 2,
+              height: View.AVATAR_SIZE * 2,
+            }}
+            variant="square"
+            aria-label="recipe"
+            src={
+              user.avatar_photo
+                ? "data:image/*;base64," + user.avatar_photo
+                : STRING.EMPTY
+            }
+            alt={user.username}
+          ></Avatar>
+        </DialogContent>
+        <DialogContent>
+          <TextField margin="dense" type="file" onChange={onChange} fullWidth />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseFormUpload}>{cancelBtnLabel}</Button>
+          <Button onClick={handleCloseFormUpload}>{submitBtnLabel}</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
+
+ProfileHeader.propTypes = {
+  uploadTitle: PropTypes.string,
+  cancelBtnLabel: PropTypes.string,
+  submitBtnLabel: PropTypes.string,
+};
+
+ProfileHeader.defaultProps = {
+  uploadTitle: "Upload your avatar",
+  cancelBtnLabel: "Cancel",
+  submitBtnLabel: "Okay",
+};
